2e687bdddec19398b229a57322acc21a1f15ce49
Comst <750145240@qq.com>
2018-03-15 08:05:36 +0800

    init project

---
+++ LZGLESPro_00/LZGLESPro_00/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZGLESPro_00
+//
+//  Created by comst on 2018/3/15.
+//  Copyright © 2018年 comst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
